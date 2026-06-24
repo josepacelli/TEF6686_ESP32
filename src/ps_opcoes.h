96109f7044c885c138a5cc4c77b320866bdab65e
jose pacelli <josepacelli@gmail.com>
2026-06-23 21:32:03 -0300

    Add radio options and related text for user interface

---
+++ src/ps_opcoes.h
@@ -0,0 +1,7 @@
+#ifndef PS_OPCOES_H
+#define PS_OPCOES_H
+
+extern const char* PS_OPCOES[];
+extern const int PS_OPCOES_COUNT;
+
+#endif
---
+++ src/rt_opcoes.h
@@ -0,0 +1,7 @@
+#ifndef RT_OPCOES_H
+#define RT_OPCOES_H
+
+extern const char* RT_OPCOES[];
+extern const int RT_OPCOES_COUNT;
+
+#endif
